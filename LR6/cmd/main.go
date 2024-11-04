304665574d7b9339f2d973a7e5cd3517a593126e
Eldar Mavlyukaev <113362672+mavlyukaev@users.noreply.github.com>
2024-11-04 22:38:28 +0300

    Update main.go
    Renaming a repository link

--- LR6/cmd/main.go
+++ LR6/cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/mavlyukaev/7S-cloud-technologies-and-backend/internal/worker"
+	"github.com/mavlyukaev/cloud-technologies-and-backend/internal/worker"
 )
 
 func main() {
