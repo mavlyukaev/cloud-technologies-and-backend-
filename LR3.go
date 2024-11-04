0c58e2188ed340b5b3db1574fd799b5c92529bf0
Eldar Mavlyukaev <113362672+mavlyukaev@users.noreply.github.com>
2024-11-04 22:12:16 +0300

    Create LR3
    Задание: Напишите программу на языке Go, которая выполняет следующую задачу:
    
    1. Создайте свою реализацию мьютекса с использованием каналов для синхронизации горутин.
    2. Запустите несколько горутин (например, 3), каждая из которых должна выполнить определенное действие (например, вывести сообщение в консоль).
    3. Используйте мьютекс для ожидания завершения всех горутин перед завершением выполнения основной горутины.
    4. Ваша программа должна использовать мьютекс для синхронизации горутин с помощью канала сигналов.
    
    Подсказка: Важно корректно использовать каналы для передачи сигналов между горутинами и основным потоком, чтобы гарантировать завершение всех горутин перед завершением программы.

---
+++ LR3.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Mutex struct {
+	Count  int
+	signal chan struct{}
+}
+
+func (m *Mutex) Unlock() {
+	m.signal <- struct{}{}
+}
+
+func (m *Mutex) Wait() {
+	for i := 0; i < m.Count; i++ {
+		<-m.signal
+	}
+}
+
+func main() {
+	m := Mutex{
+		Count:  3,
+		signal: make(chan struct{}, 3),
+	}
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			defer m.Unlock()
+			fmt.Printf("Горутина %d: Привет, мир!\n", i)
+		}()
+	}
+	m.Wait()
+}
